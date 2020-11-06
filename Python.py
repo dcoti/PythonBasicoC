75fe70ad0aff3abcb3e5e335fc9d0dee4489eb5e
Daniel Coti <progra.coti@gmail.com>
2020-11-06 14:15:55 -0600

    recursividad
    recursividad

--- Python.py
+++ Python.py
@@ -1,1 +1,17 @@
-print("Hola mundo")
+contador=2
+
+def juego(contador):
+
+    entrada = input("\n¿Cuál es el color de la fruta en la que pienso?: \n")
+    if str(entrada) != "naranja" :
+        if contador>0:
+            contador = contador-1
+            print("\n te equivocaste te quedan "+ str(contador+1)+" intentos.\n Prueba otra vez:")
+            juego(contador)
+        else:
+            print("\n GAME OVER ya no tienes mas intentos \n")
+    elif str(entrada) =="naranja" :
+        
+        print(" \n¡Felicidades adivinaste el color!\n ")     
+       
+juego(contador)
